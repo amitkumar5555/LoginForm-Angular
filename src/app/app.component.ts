a0a30c4a724bee2c5406f6d093c72f8929355f01
Amit Kumar <amit5555.kr@gmail.com>
2023-10-10 16:35:49 +0530

    Login Form In Angular With BootStrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Login Form';
+  loginForm = new FormGroup({
+    user : new FormControl(""),
+    password : new FormControl("")
+  });
+
+  loginUser(){
+    console.warn(this.loginForm.value);
+  }
+
+}
